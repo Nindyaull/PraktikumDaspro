41186466474fd303caf61646edb2ce8861e9402c
Nindyaull <pindangoreng123@gmail.com>
2025-10-10 10:19:24 +0700

    Tugas 1

---
+++ jobsheet5/ternaryOperatorCetakKRS22.java
@@ -0,0 +1,19 @@
+package jobsheet5;
+
+import java.util.Scanner;
+
+public class ternaryOperatorCetakKRS22 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("--- Cetak KRS SIAKAD ---");
+        System.out.print("Apakah UKT sudah lunas? (true/false): ");
+        boolean uktLunas = sc.nextBoolean();
+
+        String pesan = uktLunas
+                ? "Pembayaran UKT terverifikasi\nSilakan cetak KRS dan minta tanda tangan DPA"
+                : "Registrasi ditolak\nSilakan lunasi UKT terlebih dahulu";
+        
+        System.out.println(pesan);
+    }
+}
