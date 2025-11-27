a9bfe70b412c2d1ed2030254821de25d3e875adb
Nindyaull <pindangoreng123@gmail.com>
2025-11-27 16:23:33 +0700

    bioskop modif 1

--- jobsheet11/Bioskop22.java
+++ jobsheet11/Bioskop22.java
@@ -10,18 +10,22 @@ public class Bioskop22 {
         penonton[2][0] = "Eka";
         penonton[2][1] = "Farah";
         penonton[3][0] = "Gisel";
-        // penonton[3][1] = "Hana";
+        penonton[3][1] = "Hana";
 
-        // System.out.println(penonton.length);
+        System.out.println(penonton.length);
+
+        for (int i = 0; i < penonton.length; i++) {
+            System.out.println("Panjang baris ke-" + (i+1) + ": " + penonton[i].length);
+        }
         // System.out.println(penonton[0].length);
         // System.out.println(penonton[1].length);
         // System.out.println(penonton[2].length);
         // System.out.println(penonton[3].length);
 
-        System.out.printf("%s \t %s\n", penonton[0][0], penonton[0][1]);
-        System.out.printf("%s \t %s\n", penonton[1][0], penonton[1][1]);
-        System.out.printf("%s \t %s\n", penonton[2][0], penonton[2][1]);
-        System.out.printf("%s \t %s\n", penonton[3][0], penonton[3][1]);
+        // System.out.printf("%s \t %s\n", penonton[0][0], penonton[0][1]);
+        // System.out.printf("%s \t %s\n", penonton[1][0], penonton[1][1]);
+        // System.out.printf("%s \t %s\n", penonton[2][0], penonton[2][1]);
+        // System.out.printf("%s \t %s\n", penonton[3][0], penonton[3][1]);
     }
     
 }
