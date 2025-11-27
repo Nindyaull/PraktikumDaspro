a1d29c036b6a694019b5cfa2f37ce1dda5a051cb
Nindyaull <pindangoreng123@gmail.com>
2025-11-27 21:02:55 +0700

    bioskop modif 5

--- jobsheet11/Bioskop22.java
+++ jobsheet11/Bioskop22.java
@@ -24,17 +24,21 @@ public class Bioskop22 {
             System.out.println("Panjang baris: " + barisPenonton.length);
         }
 
+        for (int i = 0; i < penonton.length; i++) {
+            System.out.println("Penonton pada baris ke-" + (i+1) + ": " + String.join(", ", penonton[i]));
+        }
         // System.out.println("Penonton pada baris ke-3: ");
 
         // for (int i = 0; i < penonton[2].length; i++) {
         //     System.out.println(penonton[2][i]);
         // }
 
-        System.out.println("Penonton pada baris ke-3: ");
+        // System.out.println("Penonton pada baris ke-3: ");
 
-        for (String i : penonton[2]) {
-            System.out.println(i);
-        }
+        // for (String i : penonton[2]) {
+        //     System.out.println(i);
+        // }
+
         // System.out.println(penonton[0].length);
         // System.out.println(penonton[1].length);
         // System.out.println(penonton[2].length);
