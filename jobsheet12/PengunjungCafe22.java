50225e0eb0bc51bb170f9487d88c8e33cbb73e1b
Nindyaull <pindangoreng123@gmail.com>
2025-12-04 17:51:49 +0700

    Percobaan 4 + modif

---
+++ jobsheet12/PengunjungCafe22.java
@@ -0,0 +1,18 @@
+package jobsheet12;
+
+public class PengunjungCafe22 {
+    static void daftarPengunjung(String...namaPengunjung) {
+        System.out.println("Daftar Nama Pengunjung:");
+        // for (int i = 0; i < namaPengunjung.length; i++) {
+        //     System.out.println("- " +namaPengunjung[i]);
+        // }
+        for (String a : namaPengunjung) {
+            System.out.println("-" +a);
+        }
+    }
+    public static void main(String[] args) {
+        daftarPengunjung("Ali", "Budi", "Citra");
+        daftarPengunjung("Andi");
+        daftarPengunjung("Doni", "Eti", "Fahmi", "Galih");
+    }
+}
